1ada3dd95d6259571788611920a791312120d24c
LauraToledoGutierrez <Laura.Toledo3@alu.uclm.es>
2024-01-06 22:20:16 +0100

    fin auth.go

--- docker/auth/auth.go
+++ docker/auth/auth.go
@@ -16,8 +16,7 @@ import (
 
 	"github.com/dgrijalva/jwt-go" //go get github.com/dgrijalva/jwt-go
 	"github.com/gin-gonic/gin"    //go get github.com/gin-gonic/gin
-	"github.com/google/uuid"
-	//go get github.com/google/uuid
+	"github.com/google/uuid"      //go get github.com/google/uuid
 )
 
 const (
@@ -33,14 +32,11 @@ const (
 
 var TOKENS_DICT = make(map[string]string)
 
-type Credentials struct {
-	Username string `json:"username"`
-	Password string `json:"password"`
-}
-
 type Signup struct{}
 type Login struct{}
 type Authorize struct{}
+
+// AUXILIARY FUNCTIONS
 
 func verifyUser(username string) bool {
 	for user := range TOKENS_DICT {
@@ -102,35 +98,40 @@ func verifyToken(username, token string) bool {
 	return false
 }
 
+// Check if the user directory and shadow file exist
 func checkDirectories() {
-	// Comprobar si el directorio de usuarios existe
 	if _, err := os.Stat(USERS_PATH); os.IsNotExist(err) {
 		os.Mkdir(USERS_PATH, os.ModeDir)
 	}
-
-	// Comprobar si el archivo de sombra (shadow file) existe
 	if _, err := os.Stat(".shadow"); os.IsNotExist(err) {
 		os.Create(".shadow")
 	}
 }
 
+// Encrypt the password using SHA-256 hashing with provided salt
 func encryptPassword(salt, password string) string {
-	combination := salt + password
-
-	sha := sha256.New()
-	sha.Write([]byte(combination))
-	encrypted := sha.Sum(nil)
-
-	return hex.EncodeToString(encrypted)
+	// Create a new SHA-256 hash instance
+	hash := sha256.New()
+	//Concatenate the salt and password and write it to the hash
+	hash.Write([]byte(salt + password))
+	// Get the hash in bytes
+	hashInBytes := hash.Sum(nil)
+	// Convert the hash to a hexadecimal string and return it
+	return hex.EncodeToString(hashInBytes)
 }
 
+// Generate an access token for the given username
 func generateAccessToken(username string) string {
+	// Calculate the expiration time for the token
 	expirationTime := time.Now().Add(time.Duration(TIME_EXPIRATION) * time.Minute)
+	// Create JWT claims with the username and experitation time
 	claims := jwt.MapClaims{
 		"username": username,
 		"exp":      expirationTime.Unix(),
 	}
+	// Create a new JWT token the specidgied signing method and claims
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	// Sign the token with the secret key to get a signed token string
 	tokenString, err := token.SignedString([]byte(KEY))
 	if err != nil {
 		log.Fatal(err)
@@ -138,6 +139,7 @@ func generateAccessToken(username string) string {
 	return tokenString
 }
 
+// SIGNUP -> Method to check if the user already exists in the shadow file
 func (s *Signup) checkUsername(username string) bool {
 
 	//Open the shadow file for reading
@@ -158,23 +160,29 @@ func (s *Signup) checkUsername(username string) bool {
 	return false
 }
 
+// SIGNUP -> Method of user registration
 func (s *Signup) registerUser(username string, password string) error {
+	// Open or create the shadow file for appending
 	shadowFile, err := os.OpenFile(SHADOW_FILE, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
 	defer shadowFile.Close()
 
+	// Generate a unique salt using UUID
 	salt := uuid.New().String()
+	// Encrypt the password using the generate salt
 	encryptedPassword := encryptPassword(salt, password)
 	credentials := fmt.Sprintf("%s:%s:%s\n", username, salt, encryptedPassword)
 
+	// Write the user information to the shadow file
 	if _, err := shadowFile.WriteString(credentials); err != nil {
 		return err
 	}
 	return nil
 }
 
+// SIGNUP -> Method to hadle user registration
 func (s *Signup) post(c *gin.Context) {
 	// Parse JSON input from the request body
 	var jsonInput map[string]string
@@ -183,6 +191,7 @@ func (s *Signup) post(c *gin.Context) {
 		return
 	}
 
+	// Extract username and password from the JSON input
 	username, existsU := jsonInput["username"]
 	password, existPs := jsonInput["password"]
 	if !existPs || !existsU {
@@ -196,20 +205,25 @@ func (s *Signup) post(c *gin.Context) {
 		return
 	}
 
+	// Register the user and create the user space
 	err := s.registerUser(username, password)
 	if err != nil {
 		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to register user"})
 		return
 	}
 
+	// Generate an access token for the registered user
 	token := generateAccessToken(username)
+	// Store the token in a dictionary
 	TOKENS_DICT[username] = token
 
 	c.JSON(200, gin.H{"access_token": token})
 
 }
 
+// LOGIN -> Check user credentials against a shadow file
 func (l *Login) checkCredentials(username, password string) bool {
+	// Open shadow file
 	shadowFile, err := ioutil.ReadFile(".shadow")
 	if err != nil {
 		fmt.Printf("Error reading shadow file: %v\n", err)
@@ -223,6 +237,7 @@ func (l *Login) checkCredentials(username, password string) bool {
 			fmt.Println("Skipping malformed line in shadow file")
 			continue
 		}
+		// Encrypt the provided password with the stored salt from the shadow file
 		hashedPassword := encryptPassword(credentials[1], password)
 		if credentials[0] == username && credentials[2] == hashedPassword {
 			fmt.Println("Credentials verified successfully")
@@ -246,7 +261,6 @@ func (l *Login) post(c *gin.Context) {
 
 	username, existU := jsonInput["username"]
 	password, existP := jsonInput["password"]
-
 	if !existP || !existU {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "Username and password are required"})
 		return
@@ -267,6 +281,7 @@ func (l *Login) post(c *gin.Context) {
 		if verifyToken(username, token) {
 			c.JSON(http.StatusOK, gin.H{"access_token": TOKENS_DICT[username]})
 		} else {
+			// If the token has expired, delete it and generate a new one
 			delete(TOKENS_DICT, username)
 			token = generateAccessToken(username)
 			TOKENS_DICT[username] = token
@@ -277,6 +292,7 @@ func (l *Login) post(c *gin.Context) {
 	}
 }
 
+// AUTHORIZE -> Verify if the user and token are correct
 func (a *Authorize) authorize(c *gin.Context) {
 	username := c.Query("username")
 	token := c.Query("token")
